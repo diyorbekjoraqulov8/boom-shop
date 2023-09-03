e1f1d12ee18577ede503e56fc1b57ae84823ef62
diyorbekjoraqulov8 <diyorbekjoraqulov8@gmail.com>
2023-09-03 17:34:17 +0300

    Two version edded Navigation, Middleware routes, Dynamic title

---
+++ routes/products.js
@@ -0,0 +1,24 @@
+import { Router } from "express"
+const router = Router()
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: "Boom shop"
+  })
+})
+
+router.get('/products', (req, res) => {
+  res.render('products', {
+    title: "Products shop",
+    isProducts: true,
+  })
+})
+
+router.get('/add', (req, res) => {
+  res.render('add', {
+    title: "Add product",
+    isAdd: true
+  })
+})
+
+export default router
