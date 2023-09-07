a0d03c0e2c33a7649d979b33b3167ee09d0ff1ff
diyorbekjoraqulov8 <diyorbekjoraqulov8@gmail.com>
2023-09-07 14:05:39 +0300

    Four version added Mongodb and .env, Model mongoose and Create User, Password hashed, Login Api handler, Validation, What is JWT token, User jwt handler, Dynamic navbar and Cookie, Logout

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from "express";
 import { create } from 'express-handlebars';
+import mongoose from "mongoose";
+import flash from "connect-flash";
+import session from "express-session";
+import varMiddleware from "./middleware/var.js";
+import cookieParser from "cookie-parser";
+import * as dotenv from 'dotenv';
+
+// ROUTES
 import AuthRoutes from "./routes/auth.js";
 import ProductsRoutes from "./routes/products.js";
+
+dotenv.config()
 
 const app = express()
 
@@ -13,9 +23,28 @@ app.set('views', './views')
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(cookieParser())
+app.use(session({secret: "diyorbek", resave: false, saveUninitialized: false}))
+app.use(flash())
+app.use(varMiddleware)
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-const PORT = process.env.PORT || 4100
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+
+
+const startApp = async () => {
+  try {
+      mongoose.set('strictQuery', false)
+      mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,}) 
+      console.log('Mongo connected')
+
+      const PORT = process.env.PORT || 4100
+      app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  } catch(error) {
+      console.log(error)
+      process.exit()
+  }
+}
+startApp()
--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,108 @@
 import { Router } from "express"
+import User from "../models/User.js"
+import bcrypt from "bcrypt"
+import { generateJWTToken } from "../services/token.js"
+
 const router = Router()
 
 // GET
 router.get('/login', (req, res) => {
+  if (req.cookies.token) {
+    res.redirect('/')
+    return
+  }
   res.render('login', {
     title: "Login | Boom shop",
-    isLogin: true
+    isLogin: true,
+    loginError: req.flash('loginError')
   })
 })
 
 router.get('/register', (req, res) => {
+  if (req.cookies.token) {
+    res.redirect('/')
+    return
+  }
   res.render('register', {
     title: "Register | Boom shop",
-    isRegister: true
+    isRegister: true,
+    registerError: req.flash('registerError')
   })
 })
 
-// POST
-router.post('/login', (req, res) => {
-  console.log(req.body);
+router.get('/logout', (req, res) => {
+  res.clearCookie('token')
   res.redirect('/')
 })
 
-router.post('/register', (req, res) => {
-  console.log(req.body);
+// POST
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    req.flash('loginError', "All fields is required")
+    res.redirect('/login')
+    return
+  }
+
+  const existUser = await User.findOne({email})
+  if (!existUser) {
+    req.flash('loginError', 'User not found')
+    res.redirect('/login')
+    return
+  }
+  const isPassEqual = await bcrypt.compare(password, existUser.password)
+  if (!isPassEqual) {
+    req.flash('loginError', 'Password wrong')
+    res.redirect('/login')
+    return
+  }
+
+  const token = generateJWTToken(existUser._id)
+  res.cookie('token', token, {httpOnly:true, secure:true})
+  res.redirect('/')
+})
+
+router.post('/register', async (req, res) => {
+  const { firstname, lastname, email, password } = req.body
+
+  if (!firstname) {
+    req.flash('registerError', 'First Name is required')
+    res.redirect('/register')
+    return
+  } else if (!lastname) {
+    req.flash('registerError', 'Last Name is required')
+    res.redirect('/register')
+    return
+  } else if (!email) {
+    req.flash('registerError', 'Email is required')
+    res.redirect('/register')
+    return
+  } else if (!password) {
+    req.flash('registerError', 'Password is required')
+    res.redirect('/register')
+    return
+  }
+
+  const condidate = await User.findOne({email})
+
+  if (condidate) {
+    req.flash('registerError', 'User alredy exist')
+    res.redirect('/register')
+    return
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const userData = {
+    firstName: firstname,
+    lastName: lastname,
+    email: email,
+    password: hashedPassword
+  }
+  const user = await User.create(userData)
+  const token = generateJWTToken(user._id)
+
+  res.cookie('token', token, {httpOnly:true, secure:true})
   res.redirect('/')
 })
 
